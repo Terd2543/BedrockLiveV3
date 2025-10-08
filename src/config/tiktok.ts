8b72ee1b9ff836774899b8e567738e3b585a838e
Terd2543 <terdteykk@gmail.com>
2025-10-08 08:08:45 +0700

    Update tiktok.ts

--- src/config/tiktok.ts
+++ src/config/tiktok.ts
@@ -1,11 +1,12 @@
+import { TikTokLiveServerConfig } from "bedrocklive";
 import { configDotenv } from "dotenv";
 
-configDotenv();
+configDotenv(); // โหลดค่า .env
 
 export const TIKTOK_CONFIG: TikTokLiveServerConfig = {
-    tiktokUsername: process.env.TIKTOK_USERNAME,
+    tiktokUsername: process.env.TIKTOK_USERNAME || "terdy5554", // fallback
     port: Number(process.env.PORT) || 3000,
-    waitUntilLive: process.env.WAIT_UNTIL_LIVE === 'true',
-    enableGiftInfo: process.env.ENABLE_GIFT_INFO === 'true' || true,
+    waitUntilLive: process.env.WAIT_UNTIL_LIVE === "true" || false,
+    enableGiftInfo: process.env.ENABLE_GIFT_INFO === "true" || true,
     maxReconnectAttempts: Number(process.env.MAX_RECONNECT_ATTEMPTS) || 5,
 };
